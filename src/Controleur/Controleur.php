661fda22f377480e8121714515983c92812060fa
carriered <kawaii.ow9@gmail.com>
2023-09-28 11:10:38 +0200

    initial commit

--- src/Controleur/Controleur.php
+++ src/Controleur/Controleur.php
@@ -1,10 +1,25 @@
 <?php
 namespace App\Controleur;
+use App\Modele\AlternanceExterneEtudiants;
 use App\Modele\Entreprise;
+
 class Controleur {
     public static function creerEntreprise() : void {
         $entreprise = new Entreprise($_POST["num_siret"],$_POST["nom_entreprise"],$_POST["adresse"],$_POST["telephone"],$_POST["mail"],$_POST["interlocuteur"],$_POST["code_ape"],$_POST["code_ape"],$_POST["mdp"]);
         $entreprise->sauvegarder();
+    }
+
+    // creer un offre de Stage ou d'Alternance
+    /*
+    Offre Alternance => table offre_alternance
+    Offre de stage => table offre_stage
+    */
+    public static function creerOffreEtudiantStage() : void {
+
+}
+    public static function creerOffreEtudiantAlternance() : void {
+        $alternanceExterneEtudiant = new AlternanceExterneEtudiants($_POST["idEtudiantAlternant"], $_POST["numOffreAltrenance"], $_POST["numMaitreAlternance"], $_POST["idTuteurAlternance"], $_POST["dateDebutAlternance"], $_POST["dateFinAlternance"], $_POST["remuneration"], $_POST["numSiretEntrepriseExterieur"]);
+        $alternanceExterneEtudiant -> sauvegarder();
     }
 }
 
---
+++ src/Modele/AlternanceExterneEtudiants.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Modele;
+
+class AlternanceExterneEtudiants
+{
+    private string $idEtudiantAlternant;
+
+    private int $numOffreAltrenance;
+
+    private int $numMaitreAlternance;
+
+    private int $idTuteurAlternance;
+
+    private string $dateDebutAlternance;
+    private string $dateFinAlternance;
+
+    private float $remuneration;
+
+    private string $numSiretEntrepriseExterieur;
+
+    /**
+     * @param string $idEtudiantAlternant
+     * @param int $numOffreAltrenance
+     * @param int $numMaitreAlternance
+     * @param int $idTuteurAlternance
+     * @param string $dateDebutAlternance
+     * @param string $dateFinAlternance
+     * @param float $remuneration
+     * @param string $numSiretEntrepriseExterieur
+     */
+    public function __construct(string $idEtudiantAlternant, int $numOffreAltrenance, int $numMaitreAlternance, int $idTuteurAlternance, string $dateDebutAlternance, string $dateFinAlternance, float $remuneration, string $numSiretEntrepriseExterieur)
+    {
+        $this->idEtudiantAlternant = $idEtudiantAlternant;
+        $this->numOffreAltrenance = $numOffreAltrenance;
+        $this->numMaitreAlternance = $numMaitreAlternance;
+        $this->idTuteurAlternance = $idTuteurAlternance;
+        $this->dateDebutAlternance = $dateDebutAlternance;
+        $this->dateFinAlternance = $dateFinAlternance;
+        $this->remuneration = $remuneration;
+        $this->numSiretEntrepriseExterieur = $numSiretEntrepriseExterieur;
+    }
+
+    public function sauvegarder() : void {
+        $sql = "INSERT INTO Alternance VALUES(:idEtudiantAlternantTag, :numOffreAlternanceTag, :numMaitreDeAlternanceTag, :idTuteurAlternanceTag, :dateDebutAlternanceTag, :dateFinAlternanceTag, :remunerationTag, :numSiretEntrepriseAlternanceExterieur)";
+
+        $pdoStatement = ConnexionBaseDeDonnee::getPdo()->prepare($sql);
+
+        $values = array(
+            "idEtudiantAlternantTag" => $this->idEtudiantAlternant,
+            "numOffreAlternanceTag" => $this->numOffreAltrenance,
+            "numMaitreDeAlternanceTag" => $this->numMaitreAlternance,
+            "idTuteurAlternanceTag" => $this->idTuteurAlternance,
+            "dateDebutAlternanceTag" => $this->dateDebutAlternance,
+            "dateFinAlternanceTag" => $this->dateFinAlternance,
+            "remunerationTag" => $this->remuneration,
+            "numSiretEntrepriseAlternanceExterieur" => $this->numSiretEntrepriseExterieur
+        );
+
+        $pdoStatement->execute($values);
+    }
+}
