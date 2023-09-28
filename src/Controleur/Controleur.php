83dbce57a80774d9d66d213925dcffbb1d57c410
Lisa Achour <lisa.achour@etu.umontpellier.fr>
2023-09-28 10:49:38 +0200

    ajout de consulter offre et stage + vues respectives

--- src/Controleur/Controleur.php
+++ src/Controleur/Controleur.php
@@ -1,10 +1,32 @@
 <?php
-namespace App\Controleur;
-use App\Modele\Entreprise;
+require_once "../Modele/Entreprise.php";
+require_once "../Modele/ConnexionBaseDeDonnee.php";
 class Controleur {
     public static function creerEntreprise() : void {
         $entreprise = new Entreprise($_POST["num_siret"],$_POST["nom_entreprise"],$_POST["adresse"],$_POST["telephone"],$_POST["mail"],$_POST["interlocuteur"],$_POST["code_ape"],$_POST["code_ape"],$_POST["mdp"]);
         $entreprise->sauvegarder();
+    }
+    public static function consulterOffreStage() {
+        $sql = "select * from offreDeStage";
+        $pdoStatement = ConnexionBaseDeDonnee::getPdo()->prepare($sql);
+        $offres = array();
+
+        $pdoStatement->execute($offres);
+
+
+
+        require ("../Vue/VueOffreS.php");
+    }
+
+    public static function consulterOffreAlternance() {
+        $sql = "select * from offreDeAlternance";
+        $pdoStatement = ConnexionBaseDeDonnee::getPdo()->prepare($sql);
+        $offres = array();
+
+        $pdoStatement->execute($offres);
+
+
+        require ("../Vue/VueOffreA.php");
     }
 }
 
---
+++ src/Vue/VueOffreS.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des offres de stage</title>
+</head>
+<body>
+<?php
+/** @var as $offre */
+
+echo $offre;
+?>
+</body>
+</html>
