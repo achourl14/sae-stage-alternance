e294aad59cd1d9d1cb31d36ecddf22cbece77a87
Lucas Vrignaud <lucas.vrignaud@etu.umontpellier.fr>
2023-11-21 10:03:56 +0100

    formulaire convention

--- src/Vue/vueConventions.php
+++ src/Vue/vueConventions.php
@@ -189,11 +189,159 @@
                     <input type="text" placeholder="" name="adresseEtu" maxlength="100" required>
                 </div>
                 <div class="input-box">
-                    <span class="details">46 dans la bdd</span>
-                    <input type="text" placeholder="" name="elementPedagogique" maxlength="100" required>
+                    <span class="details">Code postal de l'étudiant</span>
+                    <input type="number" placeholder="" name="codPostalEtu" maxlength="11" required>
                 </div>
-
-
+                <div class="input-box">
+                    <span class="details">Pays de l'étudiant</span>
+                    <input type="text" placeholder="" name="paysEtu" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Ville de l'étudiant</span>
+                    <input type="text" placeholder="" name="villeEtu" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <label class="details" for="convVal"> Convention validé pédagogiquement </label>
+                    <select name="conventionValidePedagogiquement" id="convVal" required>
+                        <option value="oui">Oui</option>
+                        <option value="non">Non</option>
+                    </select>
+                </div>
+                <div class="input-box">
+                    <label class="details" for="avenant"> Avenant </label>
+                    <select name="avenant" id="avenant" required>
+                        <option value="oui">Oui</option>
+                        <option value="non">Non</option>
+                    </select>
+                </div>
+                <div class="input-box">
+                    <span class="details">Détail de l'avenant</span>
+                    <input type="text" placeholder="" name="detailAvenant" maxlength="100" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Date de création de la convention</span>
+                    <input type="date" placeholder="" name="dateCreationConvention"  required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Date de modification de la convention</span>
+                    <input type="date" placeholder="" name="dateModificationConvention"  required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Origine stage</span>
+                    <input type="text" placeholder="" name="origineStage" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Nom de l'établissement</span>
+                    <input type="text" placeholder="" name="nomEtablissement" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Numéro de Siret </span>
+                    <input type="text" placeholder="" name="siret" maxlength="14" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Adresse de résidence</span>
+                    <input type="text" placeholder="" name="adresseResidence" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Adresse voie</span>
+                    <input type="text" placeholder="" name="adresseVoie" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Adresse de libellé Cedex</span>
+                    <input type="text" placeholder="" name="adresseLibCedex" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Code postal</span>
+                    <input type="Number" placeholder="" name="codePostal" maxlength="11" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Commune de l'établissement d'acceuil</span>
+                    <input type="text" placeholder="" name="communeEtabAcceuil" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Pays de l'établissement</span>
+                    <input type="text" placeholder="" name="paysEtablissement" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Statut juridique</span>
+                    <input type="text" placeholder="" name="statutJuridique" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Type de la structure</span>
+                    <input type="text" placeholder="" name="typeStructure" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Effectif</span>
+                    <input type="text" placeholder="" name="effectif" maxlength="20" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Code NAF</span>
+                    <input type="text" placeholder="" name="codeNAF" maxlength="20" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Numéro de téléphone de l'établissement</span>
+                    <input type="text" placeholder="" name="telEtablissement" maxlength="10" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Fax</span>
+                    <input type="text" placeholder="" name="fax" maxlength="20" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Adresse mail de l'établissement</span>
+                    <input type="text" placeholder="" name="mailEtablissement" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Site web</span>
+                    <input type="text" placeholder="" name="siteWeb" maxlength="200" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Nom du service d'acceuil</span>
+                    <input type="text" placeholder="" name="nomServiceAcceuil" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Résidence du service d'acceuil</span>
+                    <input type="text" placeholder="" name="residenceServiceAcceuil" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Voie du service d'acceuil</span>
+                    <input type="text" placeholder="" name="voieServiceAcceuil" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Cedex du service d'acceuil</span>
+                    <input type="text" placeholder="" name="cedexServiceAcceuil" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Code postal du service d'acceuil</span>
+                    <input type="number" placeholder="" name="codePostalServiceAcceuil" maxlength="11" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Commune du service d'acceuil</span>
+                    <input type="text" placeholder="" name="communeServiceAcceuil" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Pays du service d'acceuil</span>
+                    <input type="text" placeholder="" name="paysServiceAcceuil" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Nom du tuteur preofessionnel</span>
+                    <input type="text" placeholder="" name="nomTuteurProfessionnel" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Prenom du tuteur preofessionnel</span>
+                    <input type="text" placeholder="" name="prenomTuteurProfessionnel" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Adresse mail du tuteur preofessionnel</span>
+                    <input type="text" placeholder="" name="mailTuteurProfessionnel" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Numéro de téléphone du tuteur preofessionnel</span>
+                    <input type="text" placeholder="" name="telTuteurProfessionnel" maxlength="50" required>
+                </div>
+                <div class="input-box">
+                    <span class="details">Fonction du tuteur preofessionnel</span>
+                    <input type="text" placeholder="" name="fonctionTuteurProfessionnel" maxlength="50" required>
+                </div>
 
             </div>
             <div class="button">
